d3e280b462f2b0ceb6502560ceb3ba7ab9d71567
cedric christinaz <cedric@christinaz.ch>
2012-10-12 21:42:43 +0200

    New functionalities: - Delete a document - Edit a document - Generate the preview of a document (convert pdf to image)

--- src/java/ch/christinaz/filemanager/bean/DocumentBean.java
+++ src/java/ch/christinaz/filemanager/bean/DocumentBean.java
@@ -1,14 +1,35 @@
-
 package ch.christinaz.filemanager.bean;
 
 import ch.christinaz.filemanager.entity.Document;
 import ch.christinaz.filemanager.entity.Folder;
+import com.sun.pdfview.PDFFile;
+import com.sun.pdfview.PDFPage;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.PixelGrabber;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.imageio.ImageIO;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -16,33 +37,31 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class DocumentBean {
-    
+
     /**
      * contains the path where the documents are stored on the server.
      */
     public static final String DOCUMENT_DIR = "/Users/cedric/filemanager_docdir/";
-    
     @PersistenceContext(unitName = "FileManagerPU")
     private EntityManager em;
-    
-    
-    
+
     /**
      * This method adds in the database a new document.
+     *
      * @param name - name of the document (given by the user)
      * @param remark - remark attached to the document
      * @param path - name of the document on the server
      * @param folder - folder in which the document is located
      * @return - The created document.
      */
-    public Document addNewDocument(String name, String remark, String path, String originalFilename, String mimeType, Date dateDocument,  Folder folder){
+    public Document addNewDocument(String name, String remark, String path, String originalFilename, String mimeType, Date dateDocument, Folder folder) {
 
         File file = new File(DOCUMENT_DIR + path);
-        
+
         Calendar cal = Calendar.getInstance();
         cal.setTime(dateDocument);
         cal.set(Calendar.HOUR, 1);
-        
+
         Document document = new Document();
         document.setName(name);
         document.setRemark(remark);
@@ -62,6 +81,143 @@ public class DocumentBean {
         return document;
 
     }
-    
 
+    /**
+     * This method updates in the database an existing document.
+     *
+     * @param document Document to update
+     */
+    public void updateDocument(Document document) {
+        em.merge(document);
+    }
+
+    /**
+     * This method allows to delete a document
+     *
+     * @param folder - The folder to delete
+     */
+    public void deleteDocument(Document document) {
+        Document toRemoved = em.merge(document);
+        em.remove(toRemoved);
+    }
+
+    /**
+     * This methods converts the document to an image and returns the 
+     * path of the image
+     * @param document Document to convert to an image
+     * @return 
+     */
+    public String previewDocument(Document document) {
+
+        File file = new File(DOCUMENT_DIR + document.getPath());
+        RandomAccessFile raf;
+        try {
+            raf = new RandomAccessFile(file, "r");
+            FileChannel channel = raf.getChannel();
+            ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+            PDFFile pdffile = new PDFFile(buf);
+            
+            int numPgs = pdffile.getNumPages();
+            for (int i = 0; i < numPgs; i++) {
+                
+                PDFPage page = pdffile.getPage(i);
+                // get the width and height for the doc at the default zoom
+                Rectangle rect = new Rectangle(0, 0, (int) page.getBBox().getWidth(), (int) page.getBBox().getHeight());
+                     
+                Image img = page.getImage(rect.width, rect.height, // width & height
+                    rect, // clip rect
+                    null, // null for the ImageObserver
+                    true, // fill background with white
+                    true // block until drawing is done
+                    );
+                
+                BufferedImage bImg = toBufferedImage(img);
+                File yourImageFile = new File(DOCUMENT_DIR+"page_" + i + ".png");
+                ImageIO.write(bImg, "png", yourImageFile);
+                
+               
+                
+            }
+             
+
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DocumentBean.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (IOException ex) {
+            Logger.getLogger(DocumentBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+
+        return DOCUMENT_DIR+"page_0.png";
+
+
+    }
+    
+    
+    /**
+     * This method returns a buffered image with the contents of an image
+     * @param image
+     * @return 
+     */
+    private static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage) image;
+        }
+        // This code ensures that all the pixels in the image are loaded
+        image = new ImageIcon(image).getImage();
+        // Determine if the image has transparent pixels; for this method's
+        // implementation, see e661 Determining If an Image Has Transparent
+        // Pixels
+        boolean hasAlpha = hasAlpha(image);
+        // Create a buffered image with a format that's compatible with the
+        // screen
+        BufferedImage bimage = null;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            // Determine the type of transparency of the new buffered image
+            int transparency = Transparency.OPAQUE;
+            if (hasAlpha) {
+                transparency = Transparency.BITMASK;
+            }
+            // Create the buffered image
+            GraphicsDevice gs = ge.getDefaultScreenDevice();
+            GraphicsConfiguration gc = gs.getDefaultConfiguration();
+            bimage = gc.createCompatibleImage(image.getWidth(null), image.getHeight(null), transparency);
+        } catch (HeadlessException e) {
+            // The system does not have a screen
+        }
+        if (bimage == null) {
+            // Create a buffered image using the default color model
+            int type = BufferedImage.TYPE_INT_RGB;
+            if (hasAlpha) {
+                type = BufferedImage.TYPE_INT_ARGB;
+            }
+            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+        }
+        // Copy image to buffered image
+        Graphics g = bimage.createGraphics();
+        // Paint the image onto the buffered image
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+        return bimage;
+    }
+
+    private static boolean hasAlpha(Image image) {
+        // If buffered image, the color model is readily available
+        if (image instanceof BufferedImage) {
+            BufferedImage bimage = (BufferedImage) image;
+            return bimage.getColorModel().hasAlpha();
+        }
+        // Use a pixel grabber to retrieve the image's color model;
+        // grabbing a single pixel is usually sufficient
+        PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+        try {
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+        }
+        // Get the image's color model
+        ColorModel cm = pg.getColorModel();
+        return cm.hasAlpha();
+    }
 }
--- src/java/ch/christinaz/filemanager/web/FolderManagedBean.java
+++ src/java/ch/christinaz/filemanager/web/FolderManagedBean.java
@@ -5,11 +5,15 @@ import ch.christinaz.filemanager.bean.FolderBean;
 import ch.christinaz.filemanager.entity.Document;
 import ch.christinaz.filemanager.entity.Folder;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -22,21 +26,24 @@ import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.FlowEvent;
 import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.StreamedContent;
 import org.primefaces.model.TreeNode;
 
 /**
  * This Managed Bean manages the user interface. The user interfaces is mainly
- * composed by: a tree which contains the folder, a table which contains the documents
- * of the selected folder. It also contains dialogs to add new folders, rename and delete existing folders.
+ * composed by: a tree which contains the folder, a table which contains the
+ * documents of the selected folder. It also contains dialogs to add new
+ * folders, rename and delete existing folders.
+ *
  * @author cedric
  */
 @ManagedBean
 @SessionScoped
 public class FolderManagedBean implements Serializable {
-    
+
     //private static final String DOCUMENT_DIR = "/Users/cedric/filemanager_docdir/";
-    
     @EJB
     private FolderBean folderBean;
     @EJB
@@ -45,27 +52,26 @@ public class FolderManagedBean implements Serializable {
     private TreeNode root;
     private TreeNode selectedNode;
     private List<Document> documents;
-    
     private String newFolderName;   //contains the name of the new folder that the user wants to create.
-    
     private String uploadedOriginalFileName;  //contains the original file name as named in the computer of the user
     private String uploadedNewFileName;        //contains the new name of the uploaded file
     private String mimeType;
     private boolean isFileUploaded;             //if the file is uploaded, TRUE
-    
     private String newDocName;
     private String newDocRemark;
     private Date dateDocument;
-        
+    private Document selectedDocument;
+    private String docPreview;
+
     public FolderManagedBean() {
-        uploadedOriginalFileName="";
-        uploadedNewFileName="";
-        isFileUploaded=false;
+        uploadedOriginalFileName = "";
+        uploadedNewFileName = "";
+        isFileUploaded = false;
     }
-    
+
     /**
      * This method builds correctly the UI Tree. It will asks, via a EJB, the
-     * root folder of the user and it will then loop on each folder to display 
+     * root folder of the user and it will then loop on each folder to display
      * them correctly in the UI Tree.
      */
     @PostConstruct
@@ -75,12 +81,13 @@ public class FolderManagedBean implements Serializable {
 
     /**
      * This method returns to the JSF page the Root folder of the tree.
+     *
      * @return - TreeNode root folder of the tree
      */
     public TreeNode getRoot() {
         return root;
     }
-    
+
     /*
      * This method retrives the folders from the db via the EJB folderBean and
      * then create the TreeNode objects correctly.
@@ -98,10 +105,11 @@ public class FolderManagedBean implements Serializable {
         }
         return root;
     }
-    
+
     /**
      * Recursive method to create the TreeNode structure.
-     * @param currentNode 
+     *
+     * @param currentNode
      */
     private void buildTreeRecursively(TreeNode currentNode) {
         Folder folder = (Folder) (currentNode.getData());
@@ -112,19 +120,21 @@ public class FolderManagedBean implements Serializable {
             buildTreeRecursively(tnChild);
         }
     }
-    
+
     /**
      * This methods returns the list of documents that is contained in the
      * selected folder.
+     *
      * @return List of documents included in the selected folder.
      */
     public List<Document> getDocuments() {
         //The attribute documents is set in the method onFolderSelect()
         return documents;
     }
-    
+
     /**
      * Getter to get the selected node.
+     *
      * @return the selected node
      */
     public TreeNode getSelectedNode() {
@@ -133,6 +143,7 @@ public class FolderManagedBean implements Serializable {
 
     /**
      * Setter to set the selected node
+     *
      * @param selectedNode The selected node
      */
     public void setSelectedNode(TreeNode selectedNode) {
@@ -141,24 +152,29 @@ public class FolderManagedBean implements Serializable {
 
     /**
      * The method returns the name of the new folder that the user wants to add.
+     *
      * @return the name of the new folder that the user wants to add.
      */
     public String getNewFolderName() {
         return newFolderName;
     }
-    
+
     /**
      * This method sets the name of the new folder that the user wants to add.
-     * @param newFolderName the name of the new folder that the user wants to add.
+     *
+     * @param newFolderName the name of the new folder that the user wants to
+     * add.
      */
     public void setNewFolderName(String newFolderName) {
         this.newFolderName = newFolderName;
     }
 
     /**
-     * This methods returns the original (as named on the user computer) name of the uploaded file. This method
-     * is called in the step 2 of the add new document wizard.
-     * @return 
+     * This methods returns the original (as named on the user computer) name of
+     * the uploaded file. This method is called in the step 2 of the add new
+     * document wizard.
+     *
+     * @return
      */
     public String getUploadedOriginalFileName() {
         return uploadedOriginalFileName;
@@ -187,26 +203,54 @@ public class FolderManagedBean implements Serializable {
     public void setMimeType(String mimeType) {
         this.mimeType = mimeType;
     }
-    
+
     public Date getDateDocument() {
         return dateDocument;
     }
 
-   
     public void setDateDocument(Date dateDocument) {
         this.dateDocument = dateDocument;
     }
-    
-    
+
+    public Document getSelectedDocument() {
+        return selectedDocument;
+    }
+
+    public void setSelectedDocument(Document selectedDocument) {
+        this.selectedDocument = selectedDocument;
+    }
+
     /**
-     * This method is called each time the user clicks on a node of the folder tree.
-     * @param event 
+     * This method returns the preview of the selected document.
+     * @return StreamedContent that contains the image to preview.
+     */
+    public StreamedContent getDocPreview() {
+        if (docPreview != null && !docPreview.equals("")) {
+            try {
+                FileInputStream fis = new FileInputStream(docPreview);
+                return new DefaultStreamedContent(fis);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FolderManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return new DefaultStreamedContent();
+    }
+
+    public void setDocPreview(String docPreview) {
+        this.docPreview = docPreview;
+    }
+
+    /**
+     * This method is called each time the user clicks on a node of the folder
+     * tree.
+     *
+     * @param event
      */
     public void onFolderSelect(NodeSelectEvent event) {
         Folder selectedFolder = (Folder) event.getTreeNode().getData();
         documents = selectedFolder.getDocuments();
     }
-    
+
     /**
      * This method is called when the user wants to rename an existing folder.
      */
@@ -216,7 +260,7 @@ public class FolderManagedBean implements Serializable {
             expandFromNode(selectedNode);
         }
     }
-    
+
     /**
      * This method is called when the user wants to add a new folder.
      */
@@ -229,7 +273,7 @@ public class FolderManagedBean implements Serializable {
             this.newFolderName = "";
         }
     }
-    
+
     /**
      * This method is called when the user wants to delete an existing folder.
      */
@@ -246,21 +290,29 @@ public class FolderManagedBean implements Serializable {
             selectedNode = null;
         }
     }
-    
+
+    public void deleteDocument() {
+        if (selectedDocument != null) {
+            documentBean.deleteDocument(selectedDocument);
+            documents.remove(selectedDocument);
+
+        }
+    }
+
     /**
      * This method rests the dialog to create a new folder.
      */
     public void initDialog() {
         this.newFolderName = "";
-        RequestContext.getCurrentInstance().reset(":fAdd:addFolderPanel");  
+        RequestContext.getCurrentInstance().reset(":fAdd:addFolderPanel");
     }
-    
+
     /**
-     * This method is called when the wizard to add a new document is closed.
-     * It reset all the field of the wizard in order to be ready the next time
-     * the user wants to upload another document.
+     * This method is called when the wizard to add a new document is closed. It
+     * reset all the field of the wizard in order to be ready the next time the
+     * user wants to upload another document.
      */
-    public void initWizard(){
+    public void initWizard() {
         this.newDocName = "";
         this.newDocRemark = "";
         this.uploadedNewFileName = "";
@@ -272,11 +324,12 @@ public class FolderManagedBean implements Serializable {
     }
 
     /**
-     * This method is called when the user clicks on the save button to add
-     * a new document in the system.
-     * @param actionEvent 
+     * This method is called when the user clicks on the save button to add a
+     * new document in the system.
+     *
+     * @param actionEvent
      */
-    public void saveDocument(ActionEvent actionEvent){
+    public void saveDocument(ActionEvent actionEvent) {
 
         Document doc = documentBean.addNewDocument(newDocName, newDocRemark, this.uploadedNewFileName, this.uploadedOriginalFileName, this.mimeType, this.dateDocument, (Folder) selectedNode.getData());
 
@@ -288,20 +341,39 @@ public class FolderManagedBean implements Serializable {
         this.dateDocument = null;
         this.mimeType = "";
     }
-    
+
     /**
-     * This method handles the upload of a new file. It checks the file type
-     * and copies the file to its final destination. At the end, it forces the
-     * wizard to go to the next step.
-     * @param event 
+     * This method is called when the user clicks on the save button to update
+     * an existing document in the system.
+     *
+     * @param actionEvent
      */
-    public void handleFileUpload(FileUploadEvent event){
+    public void updateDocument() {
+
+        documentBean.updateDocument(this.selectedDocument);
+    }
+
+    public void previewDocument() {
+        if (selectedDocument != null) {
+            this.docPreview = documentBean.previewDocument(selectedDocument);
+        }
+
+    }
+
+    /**
+     * This method handles the upload of a new file. It checks the file type and
+     * copies the file to its final destination. At the end, it forces the
+     * wizard to go to the next step.
+     *
+     * @param event
+     */
+    public void handleFileUpload(FileUploadEvent event) {
 
         isFileUploaded = false;
 
         //Check again file type (allowed type: PDF)
         String fileType = event.getFile().getContentType();
-        if(fileType.equalsIgnoreCase("application/pdf")){        
+        if (fileType.equalsIgnoreCase("application/pdf")) {
             try {
                 //Copy file in the documents directories
                 File file = File.createTempFile("doc_", ".pdf", new File(DocumentBean.DOCUMENT_DIR));
@@ -310,42 +382,41 @@ public class FolderManagedBean implements Serializable {
                 uploadedOriginalFileName = event.getFile().getFileName();
                 mimeType = event.getFile().getContentType();
                 isFileUploaded = true;
-                dateDocument = new Date();           
+                dateDocument = new Date();
             } catch (IOException ex) {
-               isFileUploaded = false;
-            }               
-        }   
-        if(!isFileUploaded){
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "A fatal error occurs. Unable to upload file: "+event.getFile().getFileName());  
-            FacesContext.getCurrentInstance().addMessage(null, msg);  
+                isFileUploaded = false;
+            }
         }
-        else{
+        if (!isFileUploaded) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "A fatal error occurs. Unable to upload file: " + event.getFile().getFileName());
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        } else {
             //All is ok, force Next step of the wizard
             RequestContext.getCurrentInstance().execute("wiz.loadStep(wiz.cfg.steps[1], true)");
         }
     }
-   
-   /**
-    * This method manages the flow control of the wizard to add a new document.
-    * @param event
-    * @return 
-    */
-   public String onFlowProcess(FlowEvent event){
-       
-       if(event.getOldStep().equals("fileUpload") &&  !this.isFileUploaded){
-           //Error, the user does not choose any file to upload. Stay in the current step
-           FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "It is necessary to upload a document first.");  
-           FacesContext.getCurrentInstance().addMessage(null, msg);  
-           RequestContext.getCurrentInstance().update(":fUpload:msgFileUpload");  
-           return event.getOldStep();      
-       }
-       
-       //Ok, go to next step
-       this.isFileUploaded=false;
-       return event.getNewStep();
-   }
-    
-    
+
+    /**
+     * This method manages the flow control of the wizard to add a new document.
+     *
+     * @param event
+     * @return
+     */
+    public String onFlowProcess(FlowEvent event) {
+
+        if (event.getOldStep().equals("fileUpload") && !this.isFileUploaded) {
+            //Error, the user does not choose any file to upload. Stay in the current step
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "It is necessary to upload a document first.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            RequestContext.getCurrentInstance().update(":fUpload:msgFileUpload");
+            return event.getOldStep();
+        }
+
+        //Ok, go to next step
+        this.isFileUploaded = false;
+        return event.getNewStep();
+    }
+
     /*
      * This method browses form the specfied node to the root node and expand
      * each folders.
